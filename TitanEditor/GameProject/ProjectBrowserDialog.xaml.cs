de8ffd4a7922d28703a2c612bed2bf0649c3bc0c
Venkat Swaraj <sathiraju829@gmail.com>
2025-04-27 10:31:02 +0530

    Add project files.

---
+++ TitanEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TitanEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == OpenProjectButton)
+            {
+                if (CreateProjectButton.IsChecked == true)
+                {
+                    CreateProjectButton.IsChecked = false;
+                    projectBrowser.Margin = new Thickness(0, 0, 0, 0);
+                }
+                OpenProjectButton.IsChecked = true;
+            }
+            else
+            {
+                if (OpenProjectButton.IsChecked == true)
+                {
+                    OpenProjectButton.IsChecked = false;
+                    projectBrowser.Margin = new Thickness(-800, 0, 0, 0);
+                }
+                CreateProjectButton.IsChecked = true;
+            }
+        }
+    }
+}
